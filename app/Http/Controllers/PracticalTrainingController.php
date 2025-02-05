e568654eff9e0d0074184d0838a90d7a0e51bfef
MUHAMAD IKHWAN <wxn.shah@gmail.com>
2025-02-05 16:32:58 +0800

    Update image upload base64

--- app/Http/Controllers/PracticalTrainingController.php
+++ app/Http/Controllers/PracticalTrainingController.php
@@ -7,7 +7,6 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\File;
 use App\Http\Requests\ValidationInformation;
-use th3khan\Base64ToUploadedFile\Base64ToUploadedFile;
 
 class PracticalTrainingController extends Controller
 {
